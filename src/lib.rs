b7a1f9a7dc3149788ddfdaa5aa1607940c324fc7
vaidehijoshi <vaidehi.sj@gmail.com>
2018-08-10 17:25:06 -0700

    Add Rgb struct, derive Debug, Copy, Clone

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,15 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+#[derive(Debug, Copy, Clone)]
+pub struct Rgb {
+    // red between 0-255
+    pub r: u8,
+
+    // green
+    pub g: u8,
+
+    // blue
+    pub b: u8,
+}
