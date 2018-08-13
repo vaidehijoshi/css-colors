320adc4157ccc9416a2653b14fa4cf4dd488f004
vaidehijoshi <vaidehi.sj@gmail.com>
2018-08-13 15:59:09 -0700

    Impl to_css function, add initial test

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,15 @@ pub struct Rgb {
     pub b: u8,
 }
 
+impl Rgb {
+    fn to_css(&self) -> String {
+        let rgb = format!("rgb({red}, {green}, {blue})", red=self.r, green=self.g, blue=self.b);
+
+        return rgb
+    }
+}
+
+
 #[cfg(test)]
 mod rgb_tests {
     #[test]
@@ -32,8 +41,15 @@ mod rgb_tests {
         let color = ::Rgb { r: 5, g: 10, b: 15 };
         let value = println!("color: {:?}", color);
 
-        println!("color: {:?}", "color: Rgb { r: 5, g: 10, b: 15");
+        // println!("color: {:?}", "color: Rgb { r: 5, g: 10, b: 15");
         // FIXME: this won't compile
         // assert_eq!(value.to_string(), "color: Rgb { r: 5, g: 10, b: 15 ");
+    }
+
+    #[test]
+    fn can_convert_to_css() {
+        let color = ::Rgb { r: 5, g: 10, b: 15 };
+
+        assert_eq!(color.to_css(), "rgb(5, 10, 15)");
     }
 }
