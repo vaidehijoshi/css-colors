466ad07211d5b5ccbfae128c7426a934aa1fc44b
vaidehijoshi <vaidehi.sj@gmail.com>
2018-08-13 14:40:23 -0700

    Add tests for Rgb derived functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,4 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
-
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Rgb {
     // red between 0-255
     pub r: u8,
@@ -16,4 +8,32 @@ pub struct Rgb {
 
     // blue
     pub b: u8,
+}
+
+#[cfg(test)]
+mod rgb_tests {
+    #[test]
+    fn can_clone() {
+        let color = ::Rgb { r: 5, g: 10, b: 15 };
+
+        assert_eq!(color, color.clone());
+    }
+
+    #[test]
+    fn can_copy() {
+        let color = ::Rgb { r: 5, g: 10, b: 15 };
+        let copied_color = color;
+
+        assert_eq!(color, copied_color);
+    }
+
+    #[test]
+    fn can_debug() {
+        let color = ::Rgb { r: 5, g: 10, b: 15 };
+        let value = println!("color: {:?}", color);
+
+        println!("color: {:?}", "color: Rgb { r: 5, g: 10, b: 15");
+        // FIXME: this won't compile
+        // assert_eq!(value.to_string(), "color: Rgb { r: 5, g: 10, b: 15 ");
+    }
 }
