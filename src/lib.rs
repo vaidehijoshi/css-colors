691938a605ba7859ee441bba5dd60bb793a0b917
vaidehi joshi <vaidehi.sj@gmail.com>
2018-08-14 14:37:32 -0700

    Clean up

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::fmt;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
-pub struct Rgb {
+pub struct RGB {
     // red between 0-255
     pub r: u8,
 
@@ -12,15 +12,13 @@ pub struct Rgb {
     pub b: u8,
 }
 
-impl Rgb {
-    fn to_css(&self) -> String {
-        let rgb = format!("rgb({red}, {green}, {blue})", red=self.r, green=self.g, blue=self.b);
-
-        return rgb
+impl RGB {
+    pub fn to_css(&self) -> String {
+        format!("rgb({red}, {green}, {blue})", red=self.r, green=self.g, blue=self.b)
     }
 }
 
-impl fmt::Display for Rgb {
+impl fmt::Display for RGB {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "rgb({}, {}, {})", self.r, self.g, self.b)
     }
@@ -29,16 +27,18 @@ impl fmt::Display for Rgb {
 
 #[cfg(test)]
 mod rgb_tests {
+    use ::RGB;
+
     #[test]
     fn can_clone() {
-        let color = ::Rgb { r: 5, g: 10, b: 15 };
+        let color = RGB { r: 5, g: 10, b: 15 };
 
         assert_eq!(color, color.clone());
     }
 
     #[test]
     fn can_copy() {
-        let color = ::Rgb { r: 5, g: 10, b: 15 };
+        let color = RGB { r: 5, g: 10, b: 15 };
         let copied_color = color;
 
         assert_eq!(color, copied_color);
@@ -46,26 +46,24 @@ mod rgb_tests {
 
     #[test]
     fn can_debug() {
-        let color = ::Rgb { r: 5, g: 10, b: 15 };
-        let value = println!("color: {:?}", color);
+        let color = RGB { r: 5, g: 10, b: 15 };
+        let value = format!("{:?}", color);
 
-        // println!("color: {:?}", "color: Rgb { r: 5, g: 10, b: 15");
-        // FIXME: this won't compile
-        // assert_eq!(value.to_string(), "color: Rgb { r: 5, g: 10, b: 15 ");
+        assert_eq!(value, "RGB { r: 5, g: 10, b: 15 }");
     }
 
     #[test]
     fn can_convert_to_css() {
-        let color = ::Rgb { r: 5, g: 10, b: 255 };
+        let color = RGB { r: 5, g: 10, b: 255 };
 
         assert_eq!(color.to_css(), "rgb(5, 10, 255)");
     }
 
     #[test]
     fn can_print_in_css() {
-        let color = ::Rgb { r: 5, g: 10, b: 256 };
-        let printed = println!("Display: {}", color);
+        let color = RGB { r: 5, g: 10, b: 255 };
+        let printed = format!("{}", color);
 
-        // assert_eq!(printed, "rgb(5, 10, 255)");
+        assert_eq!(printed, "rgb(5, 10, 255)");
     }
 }
