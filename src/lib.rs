1c592e4756618f85230cb8da6c27a60b999ab67e
vaidehijoshi <vaidehi.sj@gmail.com>
2018-08-14 13:44:37 -0700

    Impl fmt::Display for Rgb and WIP test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Rgb {
     // red between 0-255
@@ -15,6 +17,12 @@ impl Rgb {
         let rgb = format!("rgb({red}, {green}, {blue})", red=self.r, green=self.g, blue=self.b);
 
         return rgb
+    }
+}
+
+impl fmt::Display for Rgb {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "rgb({}, {}, {})", self.r, self.g, self.b)
     }
 }
 
@@ -48,8 +56,16 @@ mod rgb_tests {
 
     #[test]
     fn can_convert_to_css() {
-        let color = ::Rgb { r: 5, g: 10, b: 15 };
+        let color = ::Rgb { r: 5, g: 10, b: 255 };
 
-        assert_eq!(color.to_css(), "rgb(5, 10, 15)");
+        assert_eq!(color.to_css(), "rgb(5, 10, 255)");
+    }
+
+    #[test]
+    fn can_print_in_css() {
+        let color = ::Rgb { r: 5, g: 10, b: 256 };
+        let printed = println!("Display: {}", color);
+
+        // assert_eq!(printed, "rgb(5, 10, 255)");
     }
 }
