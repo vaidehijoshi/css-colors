4188fd3aa279a8747cb2ca1965e1df5a5c095daf
vaidehijoshi <vaidehi.sj@gmail.com>
2018-10-02 10:48:01 -0700

    Add HSLA basic impl

--- src/lib.rs
+++ src/lib.rs
@@ -215,9 +215,72 @@ impl Color for HSL {
     }
 }
 
+#[derive(Debug, Copy, Clone, PartialEq)]
+/// A struct to represent how much hue, saturation, and luminosity should be added to create a color.
+/// Also handles alpha specifications.
+///
+/// A valid value for `h` must range between `0-360`.
+/// The saturation ranges between `0-100`, where `0` is completely desaturated, and `100` is full saturation.
+/// The luminosity ranges between `0-100`, where `0` is no light (black), and `100` is full light (white).
+///
+/// For more, see the [CSS Color Spec](https://www.w3.org/TR/2018/REC-css-color-3-20180619/#hsla-color).
+pub struct HSLA {
+    // TOD0: can i do this with only u8's? is f32 really the best type here??
+    // FIXME: make sure this panics when it exceeds upper bounds.
+    // hue
+    pub h: f32,
+
+    // saturation
+    pub s: u8,
+
+    // luminosity
+    pub l: u8,
+
+    // alpha
+    pub a: u8,
+}
+
+impl fmt::Display for HSLA {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "hsla({}, {}%, {}%, {:.02})", self.h, self.s, self.l, self.a as f32 / 255.0)
+    }
+}
+
+impl HSLA {
+    /// Transforms numerical values into a HSL struct.
+    ///
+    /// # Example
+    /// ```
+    /// use css_colors::HSLA;
+    ///
+    /// let light_salmon = HSLA::new(6.0, 93, 71, 128);
+    ///
+    /// assert_eq!(light_salmon, HSLA { h: 6.0, s: 93, l: 71, a: 128 });
+    /// ```
+    pub fn new(h: f32, s: u8, l: u8, a: u8) -> HSLA {
+        HSLA { h, s, l, a }
+    }
+}
+
+impl Color for HSLA {
+    fn to_css(self) -> String {
+        self.to_string()
+    }
+
+    fn to_rgb(self) -> RGB {
+        // FIXME: create impl, add tests for this
+        RGB::new(self.h as u8, self.s, self.l)
+    }
+
+    fn to_rgba(self) -> RGBA {
+        // FIXME: create impl, add tests for this
+        RGBA::new(self.h as u8, self.s, self.l, self.a)
+    }
+}
+
 #[cfg(test)]
 mod css_color_tests {
-    use {Color, RGB, RGBA, HSL};
+    use {Color, RGB, RGBA, HSL, HSLA};
 
     #[test]
     fn can_create_color_structs() {
@@ -237,6 +300,15 @@ mod css_color_tests {
                 h: 6.0,
                 s: 93,
                 l: 71
+            }
+        );
+        assert_eq!(
+            HSLA::new(6.0, 93, 71, 255),
+            HSLA {
+                h: 6.0,
+                s: 93,
+                l: 71,
+                a: 255
             }
         );
     }
@@ -277,10 +349,17 @@ mod css_color_tests {
             s: 93,
             l: 71,
         };
+        let hsla_color = HSLA {
+            h: 6.0,
+            s: 93,
+            l: 71,
+            a: 255,
+        };
 
         assert_eq!(rgb_color, rgb_color.clone());
         assert_eq!(rgba_color, rgba_color.clone());
         assert_eq!(hsl_color, hsl_color.clone());
+        assert_eq!(hsla_color, hsla_color.clone());
     }
 
     #[test]
@@ -305,10 +384,18 @@ mod css_color_tests {
             l: 71,
         };
         let copied_hsl_color = hsl_color;
+        let hsla_color = HSLA {
+            h: 6.0,
+            s: 93,
+            l: 71,
+            a: 255,
+        };
+        let copied_hsla_color = hsla_color;
 
         assert_eq!(rgb_color, copied_rgb_color);
         assert_eq!(rgba_color, copied_rgba_color);
         assert_eq!(hsl_color, copied_hsl_color);
+        assert_eq!(hsla_color, copied_hsla_color);
     }
 
     #[test]
@@ -331,10 +418,20 @@ mod css_color_tests {
                 l: 71,
             }
         );
+        let hsla_value = format!(
+            "{:?}",
+            HSLA {
+                h: 6.0,
+                s: 93,
+                l: 71,
+                a: 255
+            }
+        );
 
         assert_eq!(rgb_value, "RGB { r: 5, g: 10, b: 15 }");
         assert_eq!(rgba_value, "RGBA { r: 5, g: 10, b: 15, a: 255 }");
         assert_eq!(hsl_value, "HSL { h: 6.0, s: 93, l: 71 }");
+        assert_eq!(hsla_value, "HSLA { h: 6.0, s: 93, l: 71, a: 255 }");
     }
 
     #[test]
@@ -355,10 +452,17 @@ mod css_color_tests {
             s: 93,
             l: 71,
         };
+        let hsla = HSLA {
+            h: 6.0,
+            s: 93,
+            l: 71,
+            a: 255,
+        };
 
         assert_eq!(rgb.to_css(), "rgb(5, 10, 255)");
         assert_eq!(rgba.to_css(), "rgba(5, 10, 255, 1.00)");
         assert_eq!(hsl.to_css(), "hsl(6, 93%, 71%)");
+        assert_eq!(hsla.to_css(), "hsla(6, 93%, 71%, 1.00)");
     }
 
     #[test]
@@ -388,10 +492,20 @@ mod css_color_tests {
                 l: 71,
             }
         );
+        let printed_hsla = format!(
+            "{}",
+            HSLA {
+                h: 6.0,
+                s: 93,
+                l: 71,
+                a: 255,
+            }
+        );
 
         assert_eq!(printed_rgb, "rgb(5, 10, 255)");
         assert_eq!(printed_rgba, "rgba(5, 10, 255, 1.00)");
         assert_eq!(printed_hsl, "hsl(6, 93%, 71%)");
+        assert_eq!(printed_hsla, "hsla(6, 93%, 71%, 1.00)");
     }
 
     #[test]
@@ -412,10 +526,17 @@ mod css_color_tests {
             s: 93,
             l: 71,
         };
+        let hsla = HSLA {
+            h: 6.0,
+            s: 93,
+            l: 71,
+            a: 255
+        };
 
         assert_eq!("rgb(5, 10, 255)".to_owned(), format!("{}", rgb));
         assert_eq!("rgba(5, 10, 255, 0.75)".to_owned(), format!("{}", rgba));
         assert_eq!("hsl(6, 93%, 71%)".to_owned(), format!("{}", hsl));
+        assert_eq!("hsla(6, 93%, 71%, 1.00)".to_owned(), format!("{}", hsla));
     }
 
     #[test]
@@ -436,9 +557,16 @@ mod css_color_tests {
             s: 93,
             l: 71,
         };
+        let hsla = HSLA {
+            h: 6.0,
+            s: 93,
+            l: 71,
+            a: 128
+        };
 
         assert_eq!(String::from("rgb(5, 10, 255)"), rgb.to_string());
         assert_eq!(String::from("rgba(5, 10, 255, 0.50)"), rgba.to_string());
         assert_eq!(String::from("hsl(6, 93%, 71%)"), hsl.to_string());
+        assert_eq!(String::from("hsla(6, 93%, 71%, 0.50)"), hsla.to_string());
     }
 }
