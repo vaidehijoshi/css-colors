9a7df4dafb1f2bb05430e98119c27a96978ce3a3
vaidehijoshi <vaidehi.sj@gmail.com>
2018-10-30 12:29:27 -0700

    More conversion tests
    Currently, some conversions seem to fail between hsl and rgb. These
    tests should hopefully help us debug what's going on.

--- src/lib.rs
+++ src/lib.rs
@@ -859,6 +859,45 @@ mod css_color_tests {
     }
 
     #[test]
+    fn can_convert_between_notations() {
+        // Test with brown
+        let rgb_brown = RGB::new(172, 96, 83);
+        let rgba_brown = RGBA::new(172, 96, 83, 255);
+        let hsl_brown = HSL::new(9, 35, 50);
+        let hsla_brown = HSLA::new(9, 35, 50, 255);
+
+        // Test with pink
+        let rgb_pink = RGB::new(253, 216, 229);
+        let rgba_pink = RGBA::new(253, 216, 229, 255);
+        let hsl_pink = HSL::new(340, 89, 92);
+        let hsla_pink = HSLA::new(340, 89, 92, 255);
+
+        // RGB
+        assert_eq!(rgb_brown.to_hsl(), hsl_brown);
+        assert_eq!(rgb_brown.to_hsla(), hsla_brown);
+        // assert_eq!(rgb_pink.to_hsl(), hsl_pink); // fails
+        // assert_eq!(rgb_pink.to_hsla(), hsla_pink); // fails
+
+        // RGBA
+        assert_eq!(rgba_brown.to_hsl(), hsl_brown);
+        assert_eq!(rgba_brown.to_hsla(), hsla_brown);
+        // assert_eq!(rgba_pink.to_hsl(), hsl_pink); // fails
+        // assert_eq!(rgba_pink.to_hsla(), hsla_pink); // fails
+
+        // HSL
+        // assert_eq!(hsl_brown.to_rgb(), rgb_brown); // fails
+        // assert_eq!(hsl_brown.to_rgba(), rgba_brown); // fails
+        // assert_eq!(hsl_pink.to_rgb(), rgb_pink); // fails
+        // assert_eq!(hsla_pink.to_rgba(), rgba_pink); // fails
+
+        // HSLA
+        // assert_eq!(hsla_brown.to_rgb(), rgb_brown); // fails
+        // assert_eq!(hsla_brown.to_rgba(), rgba_brown); // fails
+        // assert_eq!(hsla_pink.to_rgb(), rgb_pink); // fails
+        // assert_eq!(hsla_pink.to_rgba(), rgba_pink); // fails
+    }
+
+    #[test]
     fn can_clone() {
         let rgb_color = RGB::new(5, 10, 15);
         let rgba_color = RGBA::new(5, 10, 15, 255);
